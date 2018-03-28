d2beb2f9bba7b401b8efe2989b0eb3a0c2382c33
Allison <acreely@outlook.com>
2018-03-27 23:21:20 -0700

    Add files via upload

---
+++ R64-GPIO-test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# Allison Creely, 2018, LGPLv3 License
+# Rock 64 GPIO Library for Python
+
+import R64.GPIO as GPIO
+from time import sleep
+
+print("Testing R64.GPIO Module...")
+
+# Test Variables
+print("")
+print("Module Variables:")
+print("Name           Value")
+print("----           -----")
+print("GPIO.ROCK      " + str(GPIO.ROCK))
+print("GPIO.BOARD     " + str(GPIO.BOARD))
+print("GPIO.BCM       " + str(GPIO.BCM))
+print("GPIO.OUT       " + str(GPIO.OUT))
+print("GPIO.IN        " + str(GPIO.IN))
+print("GPIO.HIGH      " + str(GPIO.HIGH))
+print("GPIO.LOW       " + str(GPIO.LOW))
+print("GPIO.PUD_UP    " + str(GPIO.PUD_UP))
+print("GPIO.PUD_DOWN  " + str(GPIO.PUD_DOWN))
+print("GPIO.VERSION   " + str(GPIO.VERSION))
+print("GPIO.RPI_INFO  " + str(GPIO.RPI_INFO))
+
+print("")
+print("Testing GPIO Input/Output:")
+
+# Set Variables
+var_gpio_out1 = 16
+var_gpio_in1 = 18
+
+# GPIO Setup
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BOARD)
+
+# Test Output
+GPIO.setup(var_gpio_out1, GPIO.OUT, initial=GPIO.HIGH)       # Set up GPIO as an output, with an initial state of HIGH
+var_gpio_state = GPIO.input(var_gpio_out1)                   # Return state of GPIO
+print("Output State : " + str(var_gpio_state))               # Print results
+sleep(0.5)
+GPIO.output(var_gpio_out1, GPIO.LOW)                         # Set GPIO to LOW
+GPIO.cleanup(var_gpio_out1)                                  # Cleanup GPIO
+
+# Test Input
+GPIO.setup(var_gpio_in1, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set up GPIO as an input, pullup enabled
+var_gpio_state = GPIO.input(var_gpio_in1)                    # Return state of GPIO
+print("Input State  : " + str(var_gpio_state))               # Print results
+GPIO.cleanup(var_gpio_in1)                                   # Cleanup GPIO
+
+print("")
+print("Test Complete")
+
